72fde1becc650222425909bfc9a935b46f0d1d1a
Fang <me@fangqk.com>
2023-02-06 14:59:59 +0800

    fix(http-request): adjust test cases

--- packages/http-request/tests/HttpProxy.test.ts
+++ packages/http-request/tests/HttpProxy.test.ts
@@ -8,26 +8,16 @@ describe('Test HttpProxy.test.ts', () => {
     console.info(`Default Proxy: `, await axiosGET(url).addAxiosConfig({ proxy: false }).quickSend())
   })
 
-  it(`Test Normal Proxies`, async () => {
-    const url = 'https://ifconfig.co/ip'
-    const normalProxy = { host: '127.0.0.1', port: 19111 }
-    console.info(`Normal Proxy: `, await axiosGET(url).addAxiosConfig({ proxy: normalProxy }).quickSend())
-  })
-
-  it(`Test V2Ray HTTP Proxies`, async () => {
-    const v2rayProxy = { host: '127.0.0.1', port: 19889 }
+  it(`Test HTTP Proxies`, async () => {
+    const proxyOptions = { host: '127.0.0.1', port: 6152 }
     console.info(
       `V2Ray Proxy HTTP: `,
-      await axiosGET('http://ifconfig.co/ip').addAxiosConfig({ proxy: v2rayProxy }).quickSend()
+      await axiosGET('http://ifconfig.co/ip').addAxiosConfig({ proxy: proxyOptions }).quickSend()
     )
-    const httpsAgent = tunnel.httpsOverHttp({ proxy: v2rayProxy })
+    const httpsAgent = tunnel.httpsOverHttp({ proxy: proxyOptions })
     console.info(
       `V2Ray Proxy HTTPS: `,
       await axiosGET('https://ifconfig.co/ip').addAxiosConfig({ proxy: false, httpsAgent: httpsAgent }).quickSend()
-    )
-    console.info(
-      `V2Ray Proxy HTTPS: `,
-      await axiosGET('https://ifconfig.co/ip').addAxiosConfig({ proxy: v2rayProxy }).quickSend()
     )
   })
 })
