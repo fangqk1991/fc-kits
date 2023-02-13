85bdce3384196553e22943b545fd643e8bcd3b49
Fang <me@fangqk.com>
2023-02-13 17:52:40 +0800

    (webpack): v0.13.0 + WebpackOptionsFactory

--- packages/webpack/src/WebpackBuilder.js
+++ packages/webpack/src/WebpackBuilder.js
@@ -1,163 +1,30 @@
 const webpack = require('webpack')
-const { VueLoaderPlugin } = require('vue-loader')
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 const { merge } = require('webpack-merge')
-const tsImportPluginFactory = require('ts-import-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const TerserJSPlugin = require('terser-webpack-plugin')
-const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
-const postcss = {
-  loader: 'postcss-loader',
-  options: {
-    plugins: [require('postcss-import'), require('postcss-url'), require('autoprefixer')],
-  },
-}
-
-const generateConfigBase = () => {
-  return {
-    stats: 'normal',
-    output: {
-      filename: '[name].js',
-      publicPath: '/',
-      pathinfo: false,
-    },
-    resolve: {
-      extensions: ['.tsx', '.ts', '.js', '.vue', '.json'],
-      alias: {
-        vue$: 'vue/dist/vue.esm.js',
-      },
-    },
-    module: {
-      rules: [
-        {
-          test: /\.vue$/,
-          loader: 'vue-loader',
-        },
-        {
-          test: /\.tsx?$/,
-          loader: 'ts-loader',
-          exclude: /node_modules/,
-          options: {
-            happyPackMode: true,
-            appendTsSuffixTo: [/\.vue$/],
-            getCustomTransformers: () => ({
-              before: [
-                tsImportPluginFactory([
-                  {
-                    libraryName: 'antd',
-                    libraryDirectory: 'es',
-                    style: true,
-                  },
-                  {
-                    style: false,
-                    libraryName: 'lodash',
-                    libraryDirectory: null,
-                    camel2DashComponentName: false,
-                  },
-                ]),
-              ],
-            }),
-          },
-        },
-        {
-          test: /\.tsx?$/,
-          loader: 'ts-loader',
-          include: /node_modules\/@fangcha\/vue/,
-          options: {
-            happyPackMode: true,
-            appendTsSuffixTo: [/\.vue$/],
-            getCustomTransformers: () => ({
-              before: [
-                tsImportPluginFactory([
-                  {
-                    libraryName: 'antd',
-                    libraryDirectory: 'es',
-                    style: true,
-                  },
-                  {
-                    style: false,
-                    libraryName: 'lodash',
-                    libraryDirectory: null,
-                    camel2DashComponentName: false,
-                  },
-                ]),
-              ],
-            }),
-          },
-        },
-        {
-          test: /\.tsx?$/,
-          loader: 'ts-loader',
-          include: /node_modules\/@fangcha\/.*-(frontend|vue)/,
-          options: {
-            happyPackMode: true,
-            appendTsSuffixTo: [/\.vue$/],
-            getCustomTransformers: () => ({
-              before: [
-                tsImportPluginFactory([
-                  {
-                    libraryName: 'antd',
-                    libraryDirectory: 'es',
-                    style: true,
-                  },
-                  {
-                    style: false,
-                    libraryName: 'lodash',
-                    libraryDirectory: null,
-                    camel2DashComponentName: false,
-                  },
-                ]),
-              ],
-            }),
-          },
-        },
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-transform-runtime'],
-            cacheCompression: false,
-            cacheDirectory: true,
-          },
-        },
-      ],
-    },
-    plugins: [
-      new ForkTsCheckerWebpackPlugin({
-        typescript: {
-          extensions: {
-            vue: true,
-          },
-          diagnosticOptions: {
-            semantic: true,
-            syntactic: true,
-          },
-        },
-      }),
-      new VueLoaderPlugin(),
-      // eslint-disable-next-line
-      new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /zh/),
-      new NodePolyfillPlugin(),
-    ],
-  }
-}
+const { WebpackOptionsFactory } = require('./WebpackOptionsFactory')
 
 class WebpackBuilder {
   constructor() {
-    this.configBase = generateConfigBase()
+    this.forVue = true
     this.isDev = true
     this.port = 8080
     this.entry = {
       app: './src/app',
     }
     this.extras = {}
-    this.antdCustomVarsFile = ''
     this.aliasMap = {}
+  }
+
+  useReact() {
+    this.forVue = false
+    return this
+  }
+
+  useVue() {
+    this.forVue = true
+    return this
   }
 
   _templateFilePath = `${__dirname}/index.ejs`
@@ -172,11 +39,6 @@ class WebpackBuilder {
 
   setHtmlTitle(title) {
     this._templateParameters.htmlTitle = title
-    return this
-  }
-
-  setAntdCustomVarsFile(filePath) {
-    this.antdCustomVarsFile = filePath
     return this
   }
 
@@ -278,60 +140,7 @@ class WebpackBuilder {
         alias: this.aliasMap,
       },
       module: {
-        rules: [
-          {
-            test: /\.css$/,
-            use: ['vue-style-loader', 'css-loader', postcss],
-          },
-          {
-            test: /\.s[ac]ss$/i,
-            use: ['vue-style-loader', 'css-loader', postcss, 'sass-loader'],
-          },
-          {
-            test: /\.less$/,
-            exclude: /node_modules/,
-            use: ['vue-style-loader', 'css-loader', postcss, 'less-loader'],
-          },
-          {
-            test: /\.less$/,
-            include: /node_modules\/antd/,
-            use: [
-              'style-loader',
-              'css-loader',
-              postcss,
-              {
-                loader: 'less-loader',
-                options: {
-                  modifyVars: {
-                    hack: `true; @import "${this.antdCustomVarsFile}";`,
-                  },
-                  javascriptEnabled: true,
-                },
-              },
-            ],
-          },
-          {
-            test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-            loader: 'file-loader',
-            options: {
-              name: 'static/img/[name].[ext]',
-            },
-          },
-          {
-            test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-            loader: 'file-loader',
-            options: {
-              name: 'static/media/[name].[ext]',
-            },
-          },
-          {
-            test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-            loader: 'file-loader',
-            options: {
-              name: 'static/fonts/[name].[ext]',
-            },
-          },
-        ],
+        rules: [...WebpackOptionsFactory.cssDevRules(this.forVue), ...WebpackOptionsFactory.fileDevRules()],
       },
       plugins: [new webpack.HotModuleReplacementPlugin(), ...this.htmlPlugins()],
     }
@@ -350,106 +159,12 @@ class WebpackBuilder {
         publicPath: publicPath,
       },
       stats: 'errors-only',
-      optimization: {
-        minimizer: [new TerserJSPlugin({}), new CssMinimizerPlugin()],
-        splitChunks: {
-          chunks: 'all',
-          minSize: 30000,
-          maxSize: 0,
-          minChunks: 1,
-          maxAsyncRequests: 5,
-          maxInitialRequests: 3,
-          automaticNameDelimiter: '~',
-          name: false,
-          cacheGroups: {
-            icons: {
-              name: 'icons',
-              test: /ant-design[\\/]icons/,
-              priority: 0,
-            },
-            vendors: {
-              name: 'vendors',
-              test: /[\\/]node_modules[\\/]/,
-              priority: -10,
-            },
-            styles: {
-              name: 'styles',
-              test: /\.css$/,
-              chunks: 'all',
-              enforce: true,
-            },
-            default: {
-              minChunks: 2,
-              priority: -20,
-              reuseExistingChunk: true,
-            },
-          },
-        },
-      },
+      optimization: WebpackOptionsFactory.optimizationOptions(),
       resolve: {
         alias: this.aliasMap,
       },
       module: {
-        rules: [
-          {
-            test: /\.css$/,
-            use: [MiniCssExtractPlugin.loader, 'css-loader', postcss],
-          },
-          {
-            test: /\.scss$/,
-            use: [MiniCssExtractPlugin.loader, 'css-loader', postcss, 'sass-loader'],
-          },
-          {
-            test: /\.less$/,
-            exclude: /node_modules/,
-            use: ['vue-style-loader', 'css-loader', postcss, 'less-loader'],
-          },
-          {
-            test: /\.less$/,
-            include: /node_modules\/antd/,
-            use: [
-              'style-loader',
-              'css-loader',
-              postcss,
-              {
-                loader: 'less-loader',
-                options: {
-                  modifyVars: {
-                    hack: `true; @import "${this.antdCustomVarsFile}";`,
-                  },
-                  javascriptEnabled: true,
-                },
-              },
-            ],
-          },
-          {
-            test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-            loader: 'file-loader',
-            options: {
-              emitFile: true,
-              publicPath: publicPath,
-              name: 'static/img/[name].[ext]',
-            },
-          },
-          {
-            test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-            loader: 'file-loader',
-            options: {
-              emitFile: true,
-              publicPath: publicPath,
-              name: 'static/media/[name].[ext]',
-            },
-          },
-          {
-            test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-            loader: 'file-loader',
-            options: {
-              emitFile: true,
-              publicPath: publicPath,
-              name: 'static/fonts/[name].[ext]',
-            },
-          },
-        ],
+        rules: [...WebpackOptionsFactory.cssProdRules(), ...WebpackOptionsFactory.fileProdRules(publicPath)],
       },
       plugins: [
         new MiniCssExtractPlugin({
@@ -463,7 +178,11 @@ class WebpackBuilder {
 
   build() {
     const retainParams = this.isDev ? this._getDevRetainParams() : this._getProductionRetainParams()
-    return merge(this.configBase, retainParams, this.extras)
+    return merge(
+      this.forVue ? WebpackOptionsFactory.vueOptionsBase() : WebpackOptionsFactory.reactOptionsBase(),
+      retainParams,
+      this.extras
+    )
   }
 }
 
---
+++ packages/webpack/src/WebpackOptionsFactory.js
@@ -0,0 +1,370 @@
+const { VueLoaderPlugin } = require('vue-loader')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+const tsImportPluginFactory = require('ts-import-plugin')
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserJSPlugin = require('terser-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+
+const postcss = {
+  loader: 'postcss-loader',
+  options: {
+    plugins: [require('postcss-import'), require('postcss-url'), require('autoprefixer')],
+  },
+}
+
+class WebpackOptionsFactory {
+  static vueOptionsBase() {
+    return {
+      stats: 'normal',
+      output: {
+        filename: '[name].js',
+        publicPath: '/',
+        pathinfo: false,
+      },
+      resolve: {
+        extensions: ['.tsx', '.ts', '.js', '.vue', '.json'],
+        alias: {
+          vue$: 'vue/dist/vue.esm.js',
+        },
+      },
+      module: {
+        rules: [
+          {
+            test: /\.vue$/,
+            loader: 'vue-loader',
+          },
+          {
+            test: /\.tsx?$/,
+            loader: 'ts-loader',
+            exclude: /node_modules/,
+            options: {
+              happyPackMode: true,
+              appendTsSuffixTo: [/\.vue$/],
+              getCustomTransformers: () => ({
+                before: [
+                  tsImportPluginFactory([
+                    {
+                      libraryName: 'antd',
+                      libraryDirectory: 'es',
+                      style: true,
+                    },
+                    {
+                      style: false,
+                      libraryName: 'lodash',
+                      libraryDirectory: null,
+                      camel2DashComponentName: false,
+                    },
+                  ]),
+                ],
+              }),
+            },
+          },
+          {
+            test: /\.tsx?$/,
+            loader: 'ts-loader',
+            include: /node_modules\/@fangcha\/vue/,
+            options: {
+              happyPackMode: true,
+              appendTsSuffixTo: [/\.vue$/],
+              getCustomTransformers: () => ({
+                before: [
+                  tsImportPluginFactory([
+                    {
+                      libraryName: 'antd',
+                      libraryDirectory: 'es',
+                      style: true,
+                    },
+                    {
+                      style: false,
+                      libraryName: 'lodash',
+                      libraryDirectory: null,
+                      camel2DashComponentName: false,
+                    },
+                  ]),
+                ],
+              }),
+            },
+          },
+          {
+            test: /\.tsx?$/,
+            loader: 'ts-loader',
+            include: /node_modules\/@fangcha\/.*-(frontend|vue)/,
+            options: {
+              happyPackMode: true,
+              appendTsSuffixTo: [/\.vue$/],
+              getCustomTransformers: () => ({
+                before: [
+                  tsImportPluginFactory([
+                    {
+                      libraryName: 'antd',
+                      libraryDirectory: 'es',
+                      style: true,
+                    },
+                    {
+                      style: false,
+                      libraryName: 'lodash',
+                      libraryDirectory: null,
+                      camel2DashComponentName: false,
+                    },
+                  ]),
+                ],
+              }),
+            },
+          },
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-transform-runtime'],
+              cacheCompression: false,
+              cacheDirectory: true,
+            },
+          },
+        ],
+      },
+      plugins: [
+        new ForkTsCheckerWebpackPlugin({
+          typescript: {
+            extensions: {
+              vue: true,
+            },
+            diagnosticOptions: {
+              semantic: true,
+              syntactic: true,
+            },
+          },
+        }),
+        new VueLoaderPlugin(),
+        new NodePolyfillPlugin(),
+      ],
+    }
+  }
+
+  static reactOptionsBase() {
+    return {
+      stats: 'normal',
+      output: {
+        filename: '[name].js',
+        publicPath: '/',
+        pathinfo: false,
+      },
+      resolve: {
+        extensions: ['.tsx', '.ts', '.js', '.json'],
+        alias: {
+          vue$: 'vue/dist/vue.esm.js',
+        },
+      },
+      module: {
+        rules: [
+          {
+            test: /\.tsx?$/,
+            loader: 'ts-loader',
+            exclude: /node_modules/,
+            options: {
+              happyPackMode: true,
+            },
+          },
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-transform-runtime'],
+              cacheCompression: false,
+              cacheDirectory: true,
+            },
+          },
+        ],
+      },
+      plugins: [
+        new ForkTsCheckerWebpackPlugin({
+          typescript: {
+            diagnosticOptions: {
+              semantic: true,
+              syntactic: true,
+            },
+          },
+        }),
+        new NodePolyfillPlugin(),
+      ],
+    }
+  }
+
+  static cssDevRules(forVue) {
+    const extraLoaders = forVue ? ['vue-style-loader'] : []
+    return [
+      {
+        test: /\.css$/,
+        use: [...extraLoaders, 'css-loader', postcss],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [...extraLoaders, 'css-loader', postcss, 'sass-loader'],
+      },
+      {
+        test: /\.less$/,
+        exclude: /node_modules/,
+        use: [...extraLoaders, 'css-loader', postcss, 'less-loader'],
+      },
+      {
+        test: /\.less$/,
+        include: /node_modules\/antd/,
+        use: [
+          'style-loader',
+          'css-loader',
+          postcss,
+          // {
+          //   loader: 'less-loader',
+          //   options: {
+          //     modifyVars: {
+          //       hack: `true; @import "${this.antdCustomVarsFile}";`,
+          //     },
+          //     javascriptEnabled: true,
+          //   },
+          // },
+        ],
+      },
+    ]
+  }
+
+  static fileDevRules() {
+    return [
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'file-loader',
+        options: {
+          name: 'static/img/[name].[ext]',
+        },
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: 'file-loader',
+        options: {
+          name: 'static/media/[name].[ext]',
+        },
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'file-loader',
+        options: {
+          name: 'static/fonts/[name].[ext]',
+        },
+      },
+    ]
+  }
+
+  static cssProdRules() {
+    return [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', postcss],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', postcss, 'sass-loader'],
+      },
+      {
+        test: /\.less$/,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', postcss, 'less-loader'],
+      },
+      {
+        test: /\.less$/,
+        include: /node_modules\/antd/,
+        use: [
+          'style-loader',
+          'css-loader',
+          postcss,
+          // {
+          //   loader: 'less-loader',
+          //   options: {
+          //     modifyVars: {
+          //       hack: `true; @import "${this.antdCustomVarsFile}";`,
+          //     },
+          //     javascriptEnabled: true,
+          //   },
+          // },
+        ],
+      },
+    ]
+  }
+
+  static fileProdRules(publicPath) {
+    return [
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'file-loader',
+        options: {
+          emitFile: true,
+          publicPath: publicPath,
+          name: 'static/img/[name].[ext]',
+        },
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: 'file-loader',
+        options: {
+          emitFile: true,
+          publicPath: publicPath,
+          name: 'static/media/[name].[ext]',
+        },
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'file-loader',
+        options: {
+          emitFile: true,
+          publicPath: publicPath,
+          name: 'static/fonts/[name].[ext]',
+        },
+      },
+    ]
+  }
+
+  static optimizationOptions() {
+    return {
+      minimizer: [new TerserJSPlugin({}), new CssMinimizerPlugin()],
+      splitChunks: {
+        chunks: 'all',
+        minSize: 30000,
+        maxSize: 0,
+        minChunks: 1,
+        maxAsyncRequests: 5,
+        maxInitialRequests: 3,
+        automaticNameDelimiter: '~',
+        name: false,
+        cacheGroups: {
+          icons: {
+            name: 'icons',
+            test: /ant-design[\\/]icons/,
+            priority: 0,
+          },
+          vendors: {
+            name: 'vendors',
+            test: /[\\/]node_modules[\\/]/,
+            priority: -10,
+          },
+          styles: {
+            name: 'styles',
+            test: /\.css$/,
+            chunks: 'all',
+            enforce: true,
+          },
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true,
+          },
+        },
+      },
+    }
+  }
+}
+
+module.exports = {
+  WebpackOptionsFactory: WebpackOptionsFactory,
+}
