8b7bb35a3a2f974cc413da3916dfc14b3e2738d7
Fang <me@fangqk.com>
2023-02-13 17:52:57 +0800

    (fc-react): add project

---
+++ packages/fc-react/README.md
@@ -0,0 +1,11 @@
+# fc-react
+Components base on React.
+
+## Installation
+```
+# Use npm
+npm install @fangcha/react
+
+# Or use yarn
+yarn add @fangcha/react
+```
---
+++ packages/fc-react/webpack.dev.js
@@ -0,0 +1,7 @@
+const { WebpackBuilder } = require('@fangcha/webpack')
+
+module.exports = new WebpackBuilder()
+  .setPort(8090)
+  .setDevMode(true)
+  .setEntry('./tests/index.tsx')
+  .build()
