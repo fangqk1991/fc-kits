22645a1ec26698e8657fc184e62debc2c8a2c3aa
Fang <me@fangqk.com>
2023-02-18 13:48:25 +0800

    (backend-kit): +FeishuBot

---
+++ packages/backend-kit/src/alert/FeishuBot.ts
@@ -0,0 +1,173 @@
+import { AxiosBuilder, axiosPOST } from '@fangcha/app-request'
+import AppError from '@fangcha/app-error'
+import * as moment from 'moment'
+import { RequestFollower, ServiceProxy } from '@fangcha/tools/lib/request'
+const os = require('os')
+
+interface ApiErrorParams {
+  user: string
+  method: string
+  api: string
+  statusCode: number
+  errorMsg: string
+  reqid: string
+  duration: number
+  referer: string
+  ipAddress: string
+}
+
+export class FeishuBot extends ServiceProxy {
+  private _tag: string = ''
+  private _mute: boolean = false
+  private _hostnameHidden: boolean = false
+  private _appName: string = ''
+  private _retainedBotKey!: string
+  public readonly hostname: string
+
+  public constructor(config: {}, observerClass?: { new (requestId?: string): RequestFollower }) {
+    super(config, observerClass)
+    this.hostname = os.hostname()
+  }
+
+  public setTag(tag: string) {
+    this._tag = tag
+    return this
+  }
+
+  public setRetainedBotKey(botKey: string) {
+    this._retainedBotKey = botKey
+    return this
+  }
+
+  public setMuteMode(mute = true) {
+    this._mute = mute
+    return this
+  }
+
+  public setHostnameHidden(hostnameHidden = true) {
+    this._hostnameHidden = hostnameHidden
+    return this
+  }
+
+  public setAppName(appName: string) {
+    this._appName = appName
+    return this
+  }
+
+  protected makeRequest(botKey: string) {
+    const request = axiosPOST(`https://open.feishu.cn/open-apis/bot/v2/hook/${botKey}`).setTimeout(15000)
+    this.onRequestMade(request)
+    return request
+  }
+
+  public async sendMessage(botKey: string, message: string, mentionedList?: string[]) {
+    const extrasList: string[] = []
+    if (this._tag) {
+      extrasList.push(this._tag)
+    }
+    if (this._appName) {
+      extrasList.push(this._appName)
+    }
+    if (!this._hostnameHidden) {
+      extrasList.push(this.hostname)
+    }
+    const extras = extrasList.map((item) => `[${item}]`).join('')
+    message = `${extras} ${message}`
+    if (this._mute) {
+      console.error('sendMessage in mute-mode:', message)
+      return
+    }
+    const request = this.makeRequest(botKey)
+    const params = {
+      msg_type: 'text',
+      content: {
+        text: message,
+        // mentioned_list: mentionedList,
+      },
+    }
+    // if (Array.isArray(mentionedList) && mentionedList.length > 0) {
+    //   params.text['mentioned_list'] = mentionedList
+    //   params.text.content += '\n\n'
+    // }
+    request.setBodyData(params)
+    await request.execute()
+  }
+
+  public async sendMarkdown(botKey: string, message: string) {
+    const extrasList: string[] = []
+    if (this._tag) {
+      const tag = this._tag === 'production' ? `<font color="info">${this._tag}</font>` : this._tag
+      extrasList.push(tag)
+    }
+    if (this._appName) {
+      extrasList.push(this._appName)
+    }
+    if (!this._hostnameHidden) {
+      extrasList.push(this.hostname)
+    }
+    message = [extrasList.map((item) => `[${item}]`).join(''), `> ${message}`].join('\n')
+    if (this._mute) {
+      console.error('sendMessage in mute-mode:', message)
+      return
+    }
+    const request = this.makeRequest(botKey)
+    const params = {
+      msgtype: 'markdown',
+      markdown: {
+        content: message,
+      },
+    }
+    request.setBodyData(params)
+    await request.execute()
+  }
+
+  public async notify(message: string, atAll = false) {
+    if (!this._retainedBotKey) {
+      console.error('_retainedBotKey missing.')
+      return
+    }
+    try {
+      await this.sendMessage(this._retainedBotKey, message, atAll ? ['@all'] : [])
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  public async notifyHealthCheckingError(message: string) {
+    await this.notify(['Health Checking Error', message].join('\n'))
+  }
+
+  public async notifyServiceInvokingError(serviceName: string, client: AxiosBuilder, error: AppError) {
+    if (error.statusCode >= 500) {
+      const commonApi = client.commonApi
+      const url = client.getRequestUrl()
+      const statusCode = error.statusCode || 500
+      const errorMsg = error.message
+      const reqid = client.headers['x-request-id'] || ''
+      const infos = [
+        `Invoking Error: ${serviceName}`,
+        `Error: [${statusCode}] ${errorMsg}`,
+        `Action: ${commonApi.method} ${url}`,
+        `Reqid: ${reqid}`,
+        `Duration: ${client.getDuration()}ms`,
+        `Time: ${moment().format()}`,
+      ]
+      return this.notify(infos.join('\n'))
+    }
+  }
+
+  public async notifyApiError(params: ApiErrorParams) {
+    const infos = [
+      `Api Error`,
+      `Error: [${params.statusCode}] ${params.errorMsg}`,
+      `Action: ${params.method} ${params.api}`,
+      `User: ${params.user}`,
+      `Reqid: ${params.reqid}`,
+      `Referer: ${params.referer}`,
+      `Duration: ${params.duration}ms`,
+      `IP Address: ${params.ipAddress}`,
+      `Time: ${moment().format()}`,
+    ]
+    return this.notify(infos.join('\n'))
+  }
+}
---
+++ packages/backend-kit/tests/alert/FeishuBot.test.ts
@@ -0,0 +1,36 @@
+import * as moment from 'moment'
+import { FeishuBot } from '../../src/alert/FeishuBot'
+
+describe('Test FeishuBot.test.ts', () => {
+  const feishuBot = new FeishuBot({})
+
+  it(`Test sendMessage`, async () => {
+    await feishuBot.sendMessage(
+      'c31ef9ec-1364-4e35-b506-0400ac26d444',
+      `FeishuBot.sendMessage: ${Math.random()} at ${moment().format()}`,
+      ['@all']
+    )
+  })
+  //
+  // it(`Test sendMarkdown`, async () => {
+  //   await FeishuBot.sendMarkdown(
+  //     '44c6a32d-9505-4e53-bab1-949442e0e28b',
+  //     `FeishuBot.sendMessage: ${Math.random()} at ${moment().format()}\nFeishuBot.sendMessage: ${Math.random()} at ${moment().format()}\n123\n456`
+  //   )
+  // })
+
+  it(`Test notifyApiError`, async () => {
+    feishuBot.setRetainedBotKey('c31ef9ec-1364-4e35-b506-0400ac26d444')
+    await feishuBot.notifyApiError({
+      referer: '',
+      api: '/test/api/path',
+      method: 'GET',
+      user: 'test.user',
+      statusCode: 500,
+      errorMsg: 'Test Error Message',
+      ipAddress: '127.0.0.1',
+      reqid: 'xxx-xxx',
+      duration: 0,
+    })
+  })
+})
