d1b1795db515e4f808d37f9a77f4b882ee36773a
Fang <me@fangqk.com>
2023-04-22 17:08:48 +0800

    (fc-react): 0.18.1 - useQueryParams useMemo

--- packages/fc-react/src/hooks/useQueryParams.tsx
+++ packages/fc-react/src/hooks/useQueryParams.tsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { useSearchParams } from 'react-router-dom'
 import * as qs from 'qs'
 
 export function useQueryParams<T = {}>() {
   const [searchParams, setSearchParams] = useSearchParams()
-  const queryParams = {}
-  for (const [key, value] of searchParams.entries()) {
-    queryParams[key] = value
-  }
+  const queryParams = useMemo(() => {
+    const params = {}
+    for (const [key, value] of searchParams.entries()) {
+      params[key] = value
+    }
+    return params
+  }, [searchParams])
+
   return {
     queryParams: queryParams as T,
     setQueryParams: (params: Partial<T>) => {
