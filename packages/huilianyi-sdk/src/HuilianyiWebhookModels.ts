5c4b0bd197775e3750935ce536b6b71388e61050
Fang <me@fangqk.com>
2023-06-04 11:53:22 +0800

    (huilianyi-sdk): +HuilianyiWebhookBody

--- packages/huilianyi-sdk/src/HuilianyiWebhookModels.ts
+++ packages/huilianyi-sdk/src/HuilianyiWebhookModels.ts
@@ -1,6 +1,11 @@
 export interface HuilianyiWebhookBody {
+  apiCode: string
+  apiVersion: string
+  // tenantId: number
+
   message: string
   nonce: string
   signature: string
   timestamp: string
+  pushType: 'TENANT' | 'COMPANY'
 }
