94f39e73a61036867a2c9fdc06d9ef387aac23ef
Fang <me@fangqk.com>
2023-06-05 08:50:00 +0800

    (huilianyi-sdk): +HuilianyiWebHookService

---
+++ packages/huilianyi-sdk/src/HuilianyiWebHookService.ts
@@ -0,0 +1,38 @@
+import { HuilianyiWebhookBody } from './HuilianyiWebhookModels'
+import { HuilianyiSignatureBox } from './HuilianyiSignatureBox'
+import { HuilianyiEncryptionBox } from './HuilianyiEncryptionBox'
+import assert from '@fangcha/assert'
+
+export type HuilianyiWebhookHandler = (requestBody: HuilianyiWebhookBody) => Promise<any>
+
+interface Options {
+  tenantId: string
+  encodingAesKey: string
+  signatureToken: string
+  webhookHandler?: HuilianyiWebhookHandler
+}
+
+export class HuilianyiWebHookService {
+  private readonly options: Options
+  private readonly signatureBox: HuilianyiSignatureBox
+  private readonly encryptionBox: HuilianyiEncryptionBox
+
+  constructor(options: Options) {
+    this.options = options
+    this.signatureBox = new HuilianyiSignatureBox(options.signatureToken)
+    this.encryptionBox = new HuilianyiEncryptionBox(options.encodingAesKey, options.tenantId)
+  }
+
+  public async handle(webhookBody: HuilianyiWebhookBody) {
+    const sign = this.signatureBox.calcSignature({
+      content: webhookBody.message,
+      nonce: webhookBody.nonce,
+      timestamp: webhookBody.timestamp,
+    })
+    assert.ok(sign === webhookBody.signature, '验签失败')
+    return {
+      code: 'SUCCESS',
+      message: '',
+    }
+  }
+}
