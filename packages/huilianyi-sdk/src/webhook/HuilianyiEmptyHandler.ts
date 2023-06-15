32630f711544a39fb0393bd1d24993c7f76c16e4
Fang <me@fangqk.com>
2023-06-15 08:50:00 +0800

    (huilianyi-sdk): HuilianyiEmptyHandler +warning message

--- packages/huilianyi-sdk/src/webhook/HuilianyiEmptyHandler.ts
+++ packages/huilianyi-sdk/src/webhook/HuilianyiEmptyHandler.ts
@@ -2,6 +2,7 @@ import { HuilianyiEventHandlerBase } from './HuilianyiEventHandlerBase'
 
 export class HuilianyiEmptyHandler extends HuilianyiEventHandlerBase {
   public async onExecute() {
+    console.warn('HuilianyiEmptyHandler is invoked.')
     return {
       message: '',
     }
