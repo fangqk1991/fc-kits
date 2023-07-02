3a2663bff12109640a00290a3630099d471ca528
Fang <me@fangqk.com>
2023-07-02 08:50:00 +0800

    (huilianyi-sdk): TimeUtils + functions

--- packages/huilianyi-sdk/src/core/TimeUtils.ts
+++ packages/huilianyi-sdk/src/core/TimeUtils.ts
@@ -8,4 +8,24 @@ export class TimeUtils {
   public static timeStrUTC8(time: string) {
     return this.momentUTC8(time).format('YYYY-MM-DD HH:mm:ss')
   }
+
+  public static diff(timeStr1: string, timeStr2: string) {
+    return moment(timeStr1).valueOf() - moment(timeStr2).valueOf()
+  }
+
+  public static min(timeStr1: string, timeStr2: string) {
+    return this.diff(timeStr1, timeStr2) < 0 ? timeStr1 : timeStr2
+  }
+
+  public static max(timeStr1: string, timeStr2: string) {
+    return this.diff(timeStr1, timeStr2) > 0 ? timeStr1 : timeStr2
+  }
+
+  public static monthStartDate(time: string) {
+    return this.momentUTC8(time).startOf('month').format('YYYY-MM-DD')
+  }
+
+  public static monthEndDate(time: string) {
+    return this.momentUTC8(time).endOf('month').format('YYYY-MM-DD')
+  }
 }
