f3d74c9626470f95887725f65ee708c898007cb0
Fang <me@fangqk.com>
2023-07-20 08:50:00 +0800

    (huilianyi-sdk): +Searchers.test

---
+++ packages/huilianyi-sdk/tests/services/Searchers.test.ts
@@ -0,0 +1,20 @@
+import { HuilianyiConfigTest, HuilianyiDBTest } from '../HuilianyiConfigTest'
+import { HuilianyiService } from '../../src'
+
+describe('Test Searchers.test.ts', () => {
+  const huilianyiService = new HuilianyiService({
+    authConfig: HuilianyiConfigTest,
+    database: HuilianyiDBTest,
+  })
+
+  it(`HLY_ExpenseApplication.fc_searcher`, async () => {
+    const searcher = new huilianyiService.modelsCore.HLY_ExpenseApplication().fc_searcher({
+      'totalAmount.$lt': 1000,
+    })
+    const processor = searcher.processor()
+    processor.setColumns(['applicant_name AS applicantName', 'COUNT(*) AS count'])
+    processor.setGroupByKeys(['applicantName'])
+    const items = await processor.queryList()
+    console.info(JSON.stringify(items, null, 2))
+  })
+})
