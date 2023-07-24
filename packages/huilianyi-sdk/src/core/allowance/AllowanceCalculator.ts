f32b4fe7821d04c78768389157e2413a285bc9e7
Fang <me@fangqk.com>
2023-07-24 08:50:00 +0800

    (huilianyi-sdk): +calculateAllowanceDayItems

--- packages/huilianyi-sdk/src/core/allowance/AllowanceCalculator.ts
+++ packages/huilianyi-sdk/src/core/allowance/AllowanceCalculator.ts
@@ -10,13 +10,14 @@ export class AllowanceCalculator {
     this.rules = rules
   }
 
-  public calculateRules(roleCode: string, cityName: string) {
+  public calculateRules(roleCodeList: string[], cityName: string) {
     const result: AllowanceUnitPriceInfo = {
       unitPrice: 0,
       matchedRules: this.rules.filter((rule) => {
         const roleMatches =
-          (rule.roleMatchType === App_MatchType.Including && rule.roleList.includes(roleCode)) ||
-          (rule.roleMatchType === App_MatchType.Excluding && !rule.roleList.includes(roleCode))
+          (rule.roleMatchType === App_MatchType.Including &&
+            !!roleCodeList.find((role) => rule.roleList.includes(role))) ||
+          (rule.roleMatchType === App_MatchType.Excluding && !roleCodeList.find((role) => rule.roleList.includes(role)))
         const cityMatches =
           (rule.cityMatchType === App_MatchType.Including && rule.cityList.includes(cityName)) ||
           (rule.cityMatchType === App_MatchType.Excluding && !rule.cityList.includes(cityName))
@@ -29,44 +30,56 @@ export class AllowanceCalculator {
     return result
   }
 
-  public calculateAllowanceDayItems(roleCode: string, loopItems: App_ClosedLoop[]): AllowanceDayItem[] {
-    const dayItems: AllowanceDayItem[] = []
+  public calculateAllowanceDayItems(roleCodeList: string[], loopItems: App_ClosedLoop[]): AllowanceDayItem[] {
+    const allDayItems: AllowanceDayItem[] = []
     for (const closedLoop of loopItems) {
       // 最后一段行程按出发地，其他均按目的地
-
       const firstTicket = closedLoop.tickets[0]
       const lastTicket = closedLoop.tickets[closedLoop.tickets.length - 1]
       let curDate = TimeUtils.momentUTC8(firstTicket.fromTime).startOf('day')
+      const ruleResult = this.calculateRules(roleCodeList, firstTicket.toCity)
+
       const dayItems: AllowanceDayItem[] = [
         {
           date: curDate.format('YYYY-MM-DD'),
           cityName: firstTicket.toCity,
-          amount: 1,
-          halfDay: TimeUtils.momentUTC8(firstTicket.fromTime).hours() >= 12,
+          amount: ruleResult.unitPrice,
+          halfDay: false,
         },
       ]
       for (let i = 1; i < closedLoop.tickets.length; ++i) {
         const ticket = closedLoop.tickets[i]
         const lastCity = ticket.fromCity
+        const ruleResult = this.calculateRules(roleCodeList, lastCity)
         while (curDate.valueOf() < TimeUtils.momentUTC8(ticket.toTime).startOf('day').valueOf()) {
           curDate.add(1, 'day')
           dayItems.push({
             date: curDate.format('YYYY-MM-DD'),
             cityName: lastCity,
-            amount: 1,
+            amount: ruleResult.unitPrice,
             halfDay: false,
           })
         }
       }
+      dayItems[0].halfDay = TimeUtils.momentUTC8(firstTicket.fromTime).hours() >= 12
+      if (dayItems[0].halfDay) {
+        dayItems[0].amount /= 2
+      }
       dayItems[dayItems.length - 1].halfDay = TimeUtils.momentUTC8(lastTicket.toTime).hours() < 12
-      console.info('--- START ---')
-      console.info(dayItems)
-      console.info(closedLoop.tickets.map((item) => `${item.fromTime} ~ ${item.toTime} | ${item.fromCity} -> ${item.toCity}`))
-      console.info('--- END ---')
+      if (dayItems[dayItems.length - 1].halfDay) {
+        dayItems[dayItems.length - 1].amount /= 2
+      }
+      allDayItems.push(...dayItems)
+      // console.info('--- START ---')
+      // console.info(dayItems)
+      // console.info(
+      //   closedLoop.tickets.map((item) => `${item.fromTime} ~ ${item.toTime} | ${item.fromCity} -> ${item.toCity}`)
+      // )
+      // console.info('--- END ---')
       // for (const ticket of closedLoop.tickets) {
       //   console.info(TimeUtils.momentUTC8(ticket.fromTime), ticket.fromTime, ticket.toTime)
       // }
     }
-    return dayItems
+    return allDayItems
   }
 }
--- packages/huilianyi-sdk/tests/allowance/AllowanceCalculator.test.ts
+++ packages/huilianyi-sdk/tests/allowance/AllowanceCalculator.test.ts
@@ -13,8 +13,8 @@ describe('Test AllowanceCalculator.test.ts', () => {
   it(`AllowanceCalculator`, async () => {
     const rules = await huilianyiService.modelsCore.HLY_AllowanceRule.allRules()
     const calculator = new AllowanceCalculator(rules)
-    loggerForDev.info(calculator.calculateRules('01', '北京'))
-    loggerForDev.info(calculator.calculateRules('NULL', '北京'))
+    loggerForDev.info(calculator.calculateRules(['01'], '北京'))
+    loggerForDev.info(calculator.calculateRules(['NULL'], '北京'))
   })
 
   it(`calculateAllowanceDayItems`, async () => {
@@ -25,53 +25,64 @@ describe('Test AllowanceCalculator.test.ts', () => {
     const rules = await huilianyiService.modelsCore.HLY_AllowanceRule.allRules()
     const calculator = new AllowanceCalculator(rules)
     for (const feed of feeds) {
-      const dayItems = calculator.calculateAllowanceDayItems('', feed.extrasData().closedLoops)
+      const staff = (await huilianyiService.modelsCore.HLY_Staff.findWithUid(feed.applicantOid!))!
+      const dayItems = calculator.calculateAllowanceDayItems(staff.groupCodes(), feed.extrasData().closedLoops)
       // console.info(JSON.stringify(dayItems, null, 2))
+
+      console.info('--- START ---')
+      console.info(staff.fullName, staff.groupCodes(), dayItems)
+      // console.info(
+      //   closedLoop.tickets.map((item) => `${item.fromTime} ~ ${item.toTime} | ${item.fromCity} -> ${item.toCity}`)
+      // )
+      console.info('--- END ---')
     }
   })
 
   it(`calculateAllowanceDayItems - 2`, async () => {
     const rules = await huilianyiService.modelsCore.HLY_AllowanceRule.allRules()
     const calculator = new AllowanceCalculator(rules)
-    const dayItems = calculator.calculateAllowanceDayItems('', [
-      {
-        tickets: [
-          {
-            tagName: '机票',
-            ticketId: '',
-            trafficCode: '',
-            fromTime: '2023-07-05 20:10:00',
-            toTime: '2023-07-05 22:40:00',
-            fromCity: '上海',
-            toCity: '珠海',
-            employeeId: '',
-            employeeName: '',
-          },
-          {
-            tagName: '机票',
-            ticketId: '',
-            trafficCode: '',
-            fromTime: '2023-07-07 16:50:00',
-            toTime: '2023-07-07 19:05:00',
-            fromCity: '珠海',
-            toCity: '广州',
-            employeeId: '',
-            employeeName: '',
-          },
-          {
-            tagName: '机票',
-            ticketId: '',
-            trafficCode: '',
-            fromTime: '2023-07-11 16:50:00',
-            toTime: '2023-07-11 19:05:00',
-            fromCity: '广州',
-            toCity: '上海',
-            employeeId: '',
-            employeeName: '',
-          },
-        ],
-      },
-    ])
+    const dayItems = calculator.calculateAllowanceDayItems(
+      [],
+      [
+        {
+          tickets: [
+            {
+              tagName: '机票',
+              ticketId: '',
+              trafficCode: '',
+              fromTime: '2023-07-05 20:10:00',
+              toTime: '2023-07-05 22:40:00',
+              fromCity: '上海',
+              toCity: '珠海',
+              employeeId: '',
+              employeeName: '',
+            },
+            {
+              tagName: '机票',
+              ticketId: '',
+              trafficCode: '',
+              fromTime: '2023-07-07 16:50:00',
+              toTime: '2023-07-07 19:05:00',
+              fromCity: '珠海',
+              toCity: '广州',
+              employeeId: '',
+              employeeName: '',
+            },
+            {
+              tagName: '机票',
+              ticketId: '',
+              trafficCode: '',
+              fromTime: '2023-07-11 16:50:00',
+              toTime: '2023-07-11 19:05:00',
+              fromCity: '广州',
+              toCity: '上海',
+              employeeId: '',
+              employeeName: '',
+            },
+          ],
+        },
+      ]
+    )
   })
 
   it(`moments`, async () => {
