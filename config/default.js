485c45b55ba6615733246fd6b5fa5e33603da6e8
Fang <me@fangqk.com>
2023-07-26 08:50:00 +0800

    (oss-service): +OSSService.test

--- config/default.js
+++ config/default.js
@@ -21,4 +21,43 @@ module.exports = {
     appKey: '<username>',
     appSecurity: '<password>',
   },
+  OssDemo: {
+    demoDB: {
+      host: '127.0.0.1',
+      port: 3306,
+      database: 'demo_db',
+      username: 'root',
+      password: '',
+      dialect: 'mysql',
+      timezone: '+08:00',
+      logging: false,
+    },
+    downloadDir: `${__dirname}/../zones/demo-zone/downloads`,
+    ossOptions: {
+      Default: {
+        visitor: {
+          region: 'oss-cn-shanghai',
+          accessKeyId: '__accessKeyId__',
+          accessKeySecret: '<OSS accessKeySecret>',
+          bucket: '__bucket__',
+          secure: true,
+        },
+        uploader: {
+          region: 'oss-cn-shanghai',
+          accessKeyId: '__accessKeyId__',
+          accessKeySecret: '<OSS accessKeySecret>',
+          bucket: '__bucket__',
+          secure: true,
+        },
+        uploadSignature: {
+          accessKeyId: '__accessKeyId__',
+          accessKeySecret: '<OSS accessKeySecret>',
+          bucketURL: '<bucketURL>',
+          timeout: 3000,
+        },
+        remoteRootDir: 'datawich-staging',
+        downloadRootDir: '/data/datawich-zone/downloads',
+      },
+    },
+  },
 }
---
+++ packages/oss-service/tests/DemoXlsExportTask.ts
@@ -0,0 +1,47 @@
+import { _OSSResource, OSSService, TaskHandlerProtocol } from '../src'
+import { ResourceTaskParams } from '../src/common/models'
+import { BackendFile } from '@fangcha/tools/lib/file-backend'
+import { makeUUID } from '@fangcha/tools'
+
+interface Params {
+  options: {}
+  _userEmail: string
+  _time: string
+}
+
+export class DemoXlsExportTask implements TaskHandlerProtocol {
+  params: ResourceTaskParams
+
+  constructor(rawParams: Params) {
+    this.params = {
+      userEmail: rawParams._userEmail,
+      taskType: 'DemoXlsExportTask',
+      rawParams: rawParams,
+      fileName: `Demo_${rawParams._time}.xlsx`,
+    }
+  }
+
+  public async private_submitTask(taskKey: string) {
+    // await DatawichResque.requestResourceHandleTask(taskKey)
+  }
+
+  public async private_executeTask() {
+    const rawParams = this.params.rawParams as Params
+    const options = rawParams.options
+    const downloader = OSSService.getDownloader()
+    const file = downloader.saveContent('!!!!')
+    // const file = downloader.saveTmpFile(tmpPath)
+    const filePath = file.filePath()
+    // const fileExt = BackendFile.getFileExt(filePath)
+    // const ossKey = await ossTools.ossUtils.autoUpload(filePath, fileExt)
+    const ossKey = makeUUID()
+
+    return await _OSSResource.generateOSSResource({
+      bucketName: 'fc-demo',
+      ossKey: ossKey,
+      size: BackendFile.getFileSize(filePath),
+      uploader: rawParams._userEmail || '',
+      mimeType: BackendFile.getFileMime(filePath),
+    })
+  }
+}
