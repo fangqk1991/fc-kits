83fbcef4aa0365113634e41c4847a2b592f36ed7
Fang <me@fangqk.com>
2023-08-15 08:50:00 +0800

    (huilianyi-sdk): +TravelTools.splitTickets

--- packages/huilianyi-sdk/src/core/tools/TravelTools.ts
+++ packages/huilianyi-sdk/src/core/tools/TravelTools.ts
@@ -2,6 +2,28 @@ import * as moment from 'moment/moment'
 import { App_ClosedLoop, App_TrafficTicket } from '../travel/App_TravelModels'
 
 export class TravelTools {
+  public static splitTickets(tickets: App_TrafficTicket[]) {
+    tickets = [...tickets]
+    tickets.sort((a, b) => moment(a.fromTime).valueOf() - moment(b.toTime).valueOf())
+    const fragments: App_TrafficTicket[][] = []
+    let curTickets: App_TrafficTicket[] = []
+    for (const ticket of tickets) {
+      if (curTickets.length === 0 && ticket.fromCity !== ticket.baseCity) {
+        continue
+      }
+      if (ticket.fromCity === ticket.baseCity) {
+        curTickets = [ticket]
+        continue
+      }
+      curTickets.push(ticket)
+      if (ticket.toCity === ticket.baseCity) {
+        fragments.push(curTickets)
+        curTickets = []
+      }
+    }
+    return fragments
+  }
+
   public static makeClosedLoops(tickets: App_TrafficTicket[]): App_ClosedLoop[] | null {
     tickets = [...tickets]
     tickets.sort((a, b) => moment(a.fromTime).valueOf() - moment(b.toTime).valueOf())
--- packages/huilianyi-sdk/tests/allowance/TicketsSniffer.test.ts
+++ packages/huilianyi-sdk/tests/allowance/TicketsSniffer.test.ts
@@ -21,11 +21,38 @@ describe('Test TicketsSniffer.test.ts', () => {
     const userIdList = Object.keys(ticketMapper)
     for (const userOid of userIdList) {
       const tickets = ticketMapper[userOid]
-      // console.info(`${userOid} ${tickets.length} tickets.`)
-      const closedLoops = TravelTools.makeClosedLoops(tickets)
-      if (closedLoops) {
-        console.info(`${userOid} ${tickets.length} tickets.`)
-        console.info(JSON.stringify(closedLoops, null, 2))
+      if (!tickets[0].userOid) {
+        continue
+      }
+      // console.info(
+      //   tickets[0].userName,
+      //   tickets.map((item) => {
+      //     const fromTime = TimeUtils.momentUTC8(item.fromTime).format('MM-DD HH:mm')
+      //     const toTime = TimeUtils.momentUTC8(item.toTime).format('MM-DD HH:mm')
+      //     return `${fromTime} ~ ${toTime} ${item.trafficCode} ${item.fromCity} - ${item.toCity}`
+      //   })
+      // )
+      // console.info(
+      //   tickets[0].userName,
+      //   TravelTools.splitTickets(tickets).map((fragment) => {
+      //     return fragment.map((item) => {
+      //       const fromTime = TimeUtils.momentUTC8(item.fromTime).format('MM-DD HH:mm')
+      //       const toTime = TimeUtils.momentUTC8(item.toTime).format('MM-DD HH:mm')
+      //       return `${fromTime} ~ ${toTime} ${item.trafficCode} ${item.fromCity} - ${item.toCity}`
+      //     })
+      //   })
+      // )
+      const fragments = TravelTools.splitTickets(tickets)
+      for (const tickets of fragments) {
+        // console.info(`${userOid} ${tickets.length} tickets.`)
+        const closedLoops = TravelTools.makeClosedLoops(tickets)
+        if (closedLoops) {
+          if (tickets[0].userOid) {
+            await huilianyiService.travelService().makeDummyTravel(tickets.map((item) => item.ticketId))
+          }
+          console.info(`${userOid} ${tickets.length} tickets.`)
+          // console.info(JSON.stringify(closedLoops, null, 2))
+        }
       }
     }
   })
