99e6434ed888282ee32ad993ad41225a09fcbb07
Fang <me@fangqk.com>
2023-08-29 08:20:00 +0800

    (fc-react-test): TestExcelsView + Table Preview

--- packages/fc-react-test/src/pages/TestExcelsView.tsx
+++ packages/fc-react-test/src/pages/TestExcelsView.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { TypicalColumn, TypicalExcel } from '@fangcha/tools/src/excel'
-import { Button, Space } from 'antd'
+import { FrontendFileReader } from '@fangcha/tools/src/frontend'
+import { Button, Divider, message, Space } from 'antd'
 import { DownloadOutlined } from '@ant-design/icons'
+import { FilePickerDialog, TableView, TableViewColumn } from '@fangcha/react'
+import { TestTableView_SomeData } from './table/TestTableView_Tools'
 
 const { saveAs } = require('file-saver')
 
-const columns: TypicalColumn<any>[] = [
+interface DataProps {
+  aaa: any
+  bbb: any
+}
+
+const columns: TypicalColumn<DataProps>[] = [
   {
     columnKey: 'aaa',
     columnName: 'AAA',
@@ -17,6 +25,8 @@ const columns: TypicalColumn<any>[] = [
 ]
 
 export const TestExcelsView: React.FC = () => {
+  const [version, setVersion] = useState(0)
+  const [todoItems, setTodoItems] = useState<DataProps[]>([])
   return (
     <div>
       <Space>
@@ -32,7 +42,52 @@ export const TestExcelsView: React.FC = () => {
         >
           下载模板 <DownloadOutlined />
         </Button>
+        <Button
+          type={'primary'}
+          onClick={async () => {
+            const dialog = new FilePickerDialog({
+              title: '导入 Excel',
+            })
+            dialog.show(async (file) => {
+              const buffer = await FrontendFileReader.loadFileBuffer(file)
+              await TypicalExcel.excelFromBuffer(
+                buffer as any,
+                columns.reduce((result, field) => {
+                  result[field.columnName] = field.columnKey
+                  return result
+                }, {})
+              )
+                .then(async (excel) => {
+                  message.success(`文件解析完成`)
+                  setTodoItems(excel.records() as DataProps[])
+                  setVersion(version + 1)
+                })
+                .catch((err) => {
+                  message.error(`文件解析 / 上传失败`)
+                  throw err
+                })
+            })
+          }}
+        >
+          导入 Excel
+        </Button>
       </Space>
+      <Divider />
+      <TableView
+        version={version}
+        rowKey={(item: TestTableView_SomeData) => {
+          return item.uid
+        }}
+        columns={TableViewColumn.makeColumns<DataProps>(
+          columns.map((column) => ({
+            title: column.columnName,
+            render: (item) => <span>{item[column.columnKey]}</span>,
+          }))
+        )}
+        loadOnePageItems={async () => {
+          return todoItems
+        }}
+      />
     </div>
   )
 }
