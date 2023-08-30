31c44d37183e7f7dd4466c3748a3ebf1c4bf6517
Fang <me@fangqk.com>
2023-08-30 08:20:00 +0800

    (excel-react): ExcelPickButton + skipPreview

--- packages/fc-excel-react/src/core/ExcelPickButton.tsx
+++ packages/fc-excel-react/src/core/ExcelPickButton.tsx
@@ -6,6 +6,7 @@ import { Button, ButtonProps, message } from 'antd'
 import { FrontendFileReader } from './FrontendFileReader'
 
 interface Props<T extends object = {}> {
+  skipPreview?: boolean
   columns?: TypicalColumn<T>[]
   description?: React.ReactNode
   onPickExcel?: (excel: TypicalExcel<T>) => Promise<void> | void
@@ -15,13 +16,14 @@ export const ExcelPickButton = <T extends object = {}>({
   columns,
   description,
   onPickExcel,
+  skipPreview,
   ...props
 }: ButtonProps & Props<T>) => {
   return (
     <Button
       onClick={async () => {
         const dialog = new FilePickerDialog({
-          title: '导入 Excel',
+          title: '选择 Excel',
           description: description,
         })
         dialog.show(async (file) => {
@@ -36,19 +38,25 @@ export const ExcelPickButton = <T extends object = {}>({
           )
             .then(async (excel) => {
               message.success(`文件解析成功`)
-              new ExcelPreviewDialog({
-                columns:
-                  columns ||
-                  excel.columnKeys.map((key) => ({
-                    columnKey: key,
-                    columnName: key,
-                  })),
-                records: excel.records(),
-              }).show(async () => {
+              if (skipPreview) {
                 if (onPickExcel) {
                   await onPickExcel(excel)
                 }
-              })
+              } else {
+                new ExcelPreviewDialog({
+                  columns:
+                    columns ||
+                    excel.columnKeys.map((key) => ({
+                      columnKey: key,
+                      columnName: key,
+                    })),
+                  records: excel.records(),
+                }).show(async () => {
+                  if (onPickExcel) {
+                    await onPickExcel(excel)
+                  }
+                })
+              }
             })
             .catch((err) => {
               message.error(`文件解析失败`)
