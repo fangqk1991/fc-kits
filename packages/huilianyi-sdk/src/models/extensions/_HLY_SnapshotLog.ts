259594a96f31ad9775d3362ae84e2358a1df3856
Fang <me@fangqk.com>
2023-09-01 08:20:00 +0800

    (huilianyi-sdk): _HLY_SnapshotLog + findWithMonth

--- packages/huilianyi-sdk/src/models/extensions/_HLY_SnapshotLog.ts
+++ packages/huilianyi-sdk/src/models/extensions/_HLY_SnapshotLog.ts
@@ -4,4 +4,8 @@ export class _HLY_SnapshotLog extends __HLY_SnapshotLog {
   public constructor() {
     super()
   }
+
+  public static async findWithMonth(month: string) {
+    return (await this.findWithUid(month))!
+  }
 }
