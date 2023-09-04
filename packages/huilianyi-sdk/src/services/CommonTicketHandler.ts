7b818698e9ae25c79ce60c81fad08242f969bd8d
Fang <me@fangqk.com>
2023-09-04 08:20:00 +0800

    (huilianyi-sdk): CommonTicketHandler + updateTicket

--- packages/huilianyi-sdk/src/services/CommonTicketHandler.ts
+++ packages/huilianyi-sdk/src/services/CommonTicketHandler.ts
@@ -2,6 +2,8 @@ import { HuilianyiModelsCore } from './HuilianyiModelsCore'
 import { App_TicketParams } from '../core'
 import assert from '@fangcha/assert'
 import { HuilianyiSyncCore } from './HuilianyiSyncCore'
+import { TravelService } from './TravelService'
+import { _HLY_Travel } from '../models/extensions/_HLY_Travel'
 
 export class CommonTicketHandler {
   public readonly syncCore: HuilianyiSyncCore
@@ -16,6 +18,32 @@ export class CommonTicketHandler {
     const commonTicket = (await this.modelsCore.HLY_TrafficTicket.findWithUid(ticketId))!
     assert.ok(!!commonTicket, `票据[${ticketId}] 不存在`)
 
-    params
+    let prevTravelItem: _HLY_Travel | null = null
+    let nextTravelItem: _HLY_Travel | null = null
+
+    commonTicket.fc_edit()
+    if (params.customValid !== undefined) {
+      commonTicket.customValid = params.customValid
+    }
+    if (params.customCode !== undefined) {
+      if (commonTicket.customCode) {
+        prevTravelItem = await this.modelsCore.HLY_Travel.findWithBusinessCode(commonTicket.customCode)
+      }
+      nextTravelItem = await this.modelsCore.HLY_Travel.findWithBusinessCode(params.customCode)
+      assert.ok(!!nextTravelItem, `出差申请单[${params.customCode}] 不存在`)
+      commonTicket.customCode = params.customCode
+    }
+
+    const runner = commonTicket.dbSpec().database.createTransactionRunner()
+    await runner.commit(async (transaction) => {
+      await commonTicket.updateToDB(transaction)
+      if (prevTravelItem) {
+        await new TravelService(this.modelsCore).refreshTravelTicketsInfo(prevTravelItem, transaction)
+      }
+      if (nextTravelItem) {
+        await new TravelService(this.modelsCore).refreshTravelTicketsInfo(nextTravelItem, transaction)
+      }
+    })
+    return commonTicket.modelForClient()
   }
 }
