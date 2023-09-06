1bf523a174d38fcaa9f7b87f46d10c2fb97b58d5
Fang <me@fangqk.com>
2023-09-06 08:30:00 +0800

    (fc-react): 0.34.0 - RouterLink supports otherProps

--- packages/fc-react/src/core/router/RouterLink.tsx
+++ packages/fc-react/src/core/router/RouterLink.tsx
@@ -1,38 +1,39 @@
 import React, { ReactNode } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, LinkProps } from 'react-router-dom'
 import * as qs from 'qs'
 import * as assert from 'assert'
 
-export interface LinkProps {
+interface Props extends LinkProps {
   route: string
   params?: { [p: string]: string | number } | (string | number)[]
   queryParams?: { [p: string]: any }
   children?: ReactNode | undefined
 }
 
-export const RouterLink: React.FC<LinkProps> = (props) => {
-  const params = props.params || {}
-  const queryParams = props.queryParams || {}
-
+export const RouterLink: React.FC<Props> = ({ route, params, queryParams, children, ...props }) => {
   let pathname = ''
-  if (!props.params) {
-    pathname = props.route
+  if (!params) {
+    pathname = route
   } else if (Array.isArray(params)) {
     let index = 0
-    pathname = props.route.replace(/:([_a-zA-Z][\w-]*)/g, () => {
+    pathname = route.replace(/:([_a-zA-Z][\w-]*)/g, () => {
       return `${params[index++]}`
     })
     assert.ok(index === params.length)
   } else {
-    pathname = props.route.replace(/:([_a-zA-Z][\w-]*)/g, (_, matchStr1) => {
+    pathname = route.replace(/:([_a-zA-Z][\w-]*)/g, (_, matchStr1) => {
       return `${params[matchStr1]}`
     })
   }
   const toPath = {
     pathname: pathname,
-    search: qs.stringify(queryParams, {
+    search: qs.stringify(queryParams || {}, {
       arrayFormat: 'repeat',
     }),
   }
-  return <Link to={toPath}>{props.children}</Link>
+  return (
+    <Link {...props} to={toPath}>
+      {children}
+    </Link>
+  )
 }
