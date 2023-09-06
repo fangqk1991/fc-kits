ba8e597debf2456359b481138fed732b0433fe43
Fang <me@fangqk.com>
2023-09-06 08:30:00 +0800

    (fc-react): RouteLink supports array-like params

--- packages/fc-react/src/core/router/RouterLink.tsx
+++ packages/fc-react/src/core/router/RouterLink.tsx
@@ -1,10 +1,11 @@
 import React, { ReactNode } from 'react'
 import { Link } from 'react-router-dom'
 import * as qs from 'qs'
+import * as assert from 'assert'
 
 export interface LinkProps {
   route: string
-  params?: { [p: string]: string | number }
+  params?: { [p: string]: string | number } | (string | number)[]
   queryParams?: { [p: string]: any }
   children?: ReactNode | undefined
 }
@@ -13,10 +14,22 @@ export const RouterLink: React.FC<LinkProps> = (props) => {
   const params = props.params || {}
   const queryParams = props.queryParams || {}
 
-  const toPath = {
-    pathname: props.route.replace(/:([_a-zA-Z][\w-]*)/g, (_, matchStr1) => {
+  let pathname = ''
+  if (!props.params) {
+    pathname = props.route
+  } else if (Array.isArray(params)) {
+    let index = 0
+    pathname = props.route.replace(/:([_a-zA-Z][\w-]*)/g, () => {
+      return `${params[index++]}`
+    })
+    assert.ok(index === params.length)
+  } else {
+    pathname = props.route.replace(/:([_a-zA-Z][\w-]*)/g, (_, matchStr1) => {
       return `${params[matchStr1]}`
-    }),
+    })
+  }
+  const toPath = {
+    pathname: pathname,
     search: qs.stringify(queryParams, {
       arrayFormat: 'repeat',
     }),
