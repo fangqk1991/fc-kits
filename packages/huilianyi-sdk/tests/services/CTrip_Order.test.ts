05aac833a0dfc77a601e45bf677228804c99491d
Fang <me@fangqk.com>
2023-09-22 08:20:00 +0800

    (huilianyi-sdk): + test cases

--- packages/huilianyi-sdk/tests/services/CTrip_Order.test.ts
+++ packages/huilianyi-sdk/tests/services/CTrip_Order.test.ts
@@ -1,5 +1,6 @@
 import { HuilianyiServiceDev } from './HuilianyiServiceDev'
-import { CTrip_FlightChangeTypeDescriptor, CTrip_TrainOrderInfoEntity } from '@fangcha/ctrip-sdk'
+import { CTrip_FlightChangeTypeDescriptor, CTrip_OrderType, CTrip_TrainOrderInfoEntity } from '@fangcha/ctrip-sdk'
+import * as assert from 'assert'
 
 describe('Test CTrip_Order.test.ts', () => {
   const huilianyiService = HuilianyiServiceDev
@@ -43,6 +44,24 @@ describe('Test CTrip_Order.test.ts', () => {
         }
         if (ticketInfo.TrainTicketType === 'D') {
           console.info(`改签前: ${ticketInfo.TrainName} ${ticketInfo.DepartureDateTime}`)
+        }
+      }
+    }
+  })
+
+  it(`CTrip_Order - 3`, async () => {
+    const CTrip_Order = huilianyiService.modelsCore.CTrip_Order
+
+    const searcher = new CTrip_Order().fc_searcher()
+    searcher.processor().addConditionKV('order_type', CTrip_OrderType.TRAIN)
+    const feeds = await searcher.queryFeeds()
+    for (const item of feeds) {
+      const extrasData = item.extrasData() as CTrip_TrainOrderInfoEntity
+      for (const passenger of extrasData.PassengerInfoList) {
+        if (passenger.OrderTicket) {
+          if (passenger.OrderTicket.length !== extrasData.TicketInfoList.length) {
+            console.info(item.orderId, extrasData.BasicInfo.NewOrderStatusName)
+          }
         }
       }
     }
