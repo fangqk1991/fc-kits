561e0e43fb95353180a56f3bf5cec5fcca69d915
Fang <me@fangqk.com>
2024-04-24 20:42:48 +0800

    (fc-react): ReactApp supports routers

--- packages/fc-react-test/src/index.tsx
+++ packages/fc-react-test/src/index.tsx
@@ -1,10 +1,50 @@
-import ReactDOM from 'react-dom/client'
 import React from 'react'
-import { App } from './App'
+import { ReactApp } from '@fangcha/react'
+import { MainLayout } from './core/MainLayout'
+import { TestTableView } from './pages/table/TestTableView'
+import { TestDialogsView } from './pages/TestDialogsView'
+import { TestLoadingView } from './pages/TestLoadingView'
+import { TestWidgetsView } from './pages/TestWidgetsView'
+import { TestFormsView } from './pages/TestFormsView'
+import { TestRoutersView } from './pages/TestRoutersView'
+import { TestExcelsView } from './pages/TestExcelsView'
+import { TestTextEditor } from './pages/TestTextEditor'
 
-const app = ReactDOM.createRoot(document.getElementById('app')!)
-app.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+new ReactApp({
+  colorPrimary: 'rgb(221 115 164)',
+  mainLayout: <MainLayout />,
+  routes: [
+    {
+      path: '/v1/table-view',
+      element: <TestTableView />,
+    },
+    {
+      path: '/v1/dialogs-view',
+      element: <TestDialogsView />,
+    },
+    {
+      path: '/v1/loading-view',
+      element: <TestLoadingView />,
+    },
+    {
+      path: '/v1/widgets',
+      element: <TestWidgetsView />,
+    },
+    {
+      path: '/v1/forms',
+      element: <TestFormsView />,
+    },
+    {
+      path: '/v1/routers',
+      element: <TestRoutersView />,
+    },
+    {
+      path: '/v1/excels',
+      element: <TestExcelsView />,
+    },
+    {
+      path: '/v1/text-editor',
+      element: <TestTextEditor />,
+    },
+  ],
+}).launch()
--- packages/fc-react/src/core/ReactApp.tsx
+++ packages/fc-react/src/core/ReactApp.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouteObject, RouterProvider } from 'react-router-dom'
+import { ReactTheme } from './ReactTheme'
+import { RouteErrorBoundary } from '../error/RouteErrorBoundary'
 
 interface Params {
-  mainNode: React.ReactNode
+  colorPrimary?: string
+  mainLayout: React.ReactNode
+  routes: RouteObject[]
 }
 
 export class ReactApp {
@@ -10,11 +15,32 @@ export class ReactApp {
 
   public constructor(options: Params) {
     this.options = options
+    if (options.colorPrimary) {
+      ReactTheme.colorPrimary = options.colorPrimary
+    }
   }
 
-  public launch(children: React.ReactNode) {
+  public launch() {
     window['_app'] = this
+    const router = createBrowserRouter([
+      {
+        path: '/',
+        element: this.options.mainLayout,
+        errorElement: <RouteErrorBoundary />,
+        children: [
+          ...this.options.routes,
+          {
+            path: '*',
+            element: <div>404 Not Found</div>,
+          },
+        ],
+      },
+    ])
     const app = ReactDOM.createRoot(document.getElementById('app')!)
-    app.render(<React.StrictMode>{children}</React.StrictMode>)
+    app.render(
+      <React.StrictMode>
+        <RouterProvider router={router}></RouterProvider>
+      </React.StrictMode>
+    )
   }
 }
