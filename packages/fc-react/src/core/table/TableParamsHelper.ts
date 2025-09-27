b46971d4149d8a0320908fe0ff9ce504fff0bfed
Fang <me@fangqk.com>
2025-09-28 00:41:33 +0800

    (fc-react): + TableViewV2

---
+++ packages/fc-react/src/core/table/TableParamsHelper.ts
@@ -0,0 +1,37 @@
+export interface TablePageOptions {
+  pageNumber?: number
+  pageSize?: number
+  sortKey?: string
+  sortDirection?: 'ascend' | 'descend' | 'ascending' | 'descending' | ''
+}
+
+export interface TableDataParams {
+  _offset: number
+  _length: number
+  _sortKey: string
+  _sortDirection: string
+}
+
+export class TableParamsHelper {
+  public static extractDataParams(realSettings: TablePageOptions) {
+    const pageNumber = realSettings.pageNumber
+    const pageSize = realSettings.pageSize
+    const params: Partial<TableDataParams> = {}
+    if (pageNumber && pageSize) {
+      params._offset = (pageNumber - 1) * pageSize
+    }
+    if (pageSize) {
+      params._length = pageSize
+    }
+    if (realSettings.sortKey) {
+      params._sortKey = realSettings.sortKey
+      params._sortDirection = realSettings.sortDirection
+    }
+    if (params._sortDirection) {
+      if (['ascend', 'descend'].includes(params._sortDirection)) {
+        params._sortDirection = `${params._sortDirection}ing`
+      }
+    }
+    return params
+  }
+}
---
+++ packages/fc-react/src/core/table/TableViewV2.tsx
@@ -0,0 +1,92 @@
+import React, { PropsWithChildren, useState } from 'react'
+import { Table } from 'antd'
+import type { ColumnsType } from 'antd/es/table'
+import { TableProps } from 'antd/es/table'
+import { PageResult } from '@fangcha/tools'
+import { TablePageOptions } from './TableParamsHelper'
+
+type TableViewProtocol<T = any> = {
+  columns: ColumnsType<T>
+
+  pageResult: PageResult<T>
+  onParamsChanged?: (retainParams: Partial<TablePageOptions>) => void
+  initialSettings?: TablePageOptions
+  onDisplayItemsChanged?: (items: T[]) => void
+
+  rowKey?: string | ((record: any, index?: number) => string)
+  hidePagination?: boolean
+
+  tableProps?: TableProps<any>
+}
+
+export const TableViewV2 = <T,>(props: PropsWithChildren<TableViewProtocol<T>>) => {
+  const pageResult = props.pageResult
+
+  const [realSettings, setRealSettings] = useState(() => {
+    const settings = {
+      ...(props.initialSettings || {}),
+    }
+    settings.pageNumber = Number(settings.pageNumber || 0) || 1
+    settings.pageSize = Number(settings.pageSize || 10)
+    settings.sortKey = settings.sortKey || ''
+    settings.sortDirection = settings.sortDirection || ''
+    return settings
+  })
+
+  return (
+    <Table
+      columns={props.columns}
+      rowKey={props.rowKey}
+      scroll={{
+        x: 'max-content',
+        ...(props.tableProps?.scroll || {}),
+      }}
+      pagination={
+        !props.hidePagination && {
+          position: ['bottomRight'],
+          showSizeChanger: true,
+          // onChange: (pageNumber, pageSize) => {
+          //   const newOptions = {
+          //     ...realSettings,
+          //     pageNumber: pageNumber,
+          //     pageSize: pageSize,
+          //   }
+          //   setRealSettings(newOptions)
+          //   const retainedParams = TableParamsHelper.extractDataParams(newOptions)
+          //   props.onParamsChanged && props.onParamsChanged(retainedParams)
+          // },
+          // showTotal: props.showTotal ? (totalCount) => `Total: ${totalCount}` : undefined,
+          showTotal: (totalCount) => `TOTAL: ${totalCount}`,
+          current: realSettings.pageNumber,
+          pageSize: realSettings.pageSize,
+          total: pageResult.totalCount,
+        }
+      }
+      dataSource={pageResult.items}
+      {...(props.tableProps || {})}
+      onChange={(pagination, filters, sorter, extra) => {
+        const newOptions: any = { ...realSettings }
+        if (sorter && sorter['columnKey'] && (realSettings.sortKey || sorter['order'])) {
+          Object.assign(newOptions, {
+            sortKey: sorter['columnKey'],
+            sortDirection: sorter['order'],
+          })
+        }
+        if (pagination) {
+          Object.assign(newOptions, {
+            pageNumber: pagination.current,
+            pageSize: pagination.pageSize,
+          })
+        }
+        setRealSettings(newOptions)
+
+        props.onParamsChanged && props.onParamsChanged(newOptions)
+        props.onDisplayItemsChanged && props.onDisplayItemsChanged(extra.currentDataSource)
+
+        if (props.tableProps && props.tableProps.onChange) {
+          props.tableProps.onChange(pagination, filters, sorter, extra)
+        }
+      }}
+    />
+  )
+}
